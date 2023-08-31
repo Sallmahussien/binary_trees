4d416b9f87a4e35451bf2b2adb1f78d978ca81ec
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2023-08-31 20:11:13 +0300

    edit betty style

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * bst_helper - Checks if a binary tree is a valid Binary Search Tree
+ * binary_tree_is_bst - Checks if a binary tree is a valid Binary Search Tree
  * @tree: Pointer to the root node of the tree to check
  * Return: 1 if the tree is a valid BST, and 0 otherwise
  */
